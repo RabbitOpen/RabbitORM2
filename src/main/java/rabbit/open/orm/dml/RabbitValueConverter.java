5f2767ea9b04e4859df4185761b02a632372a00e
qb.xiao <qb.xiao@hnair.com>
2019-04-11 19:27:12 +0800

    v2.8.0

--- src/main/java/rabbit/open/orm/dml/RabbitValueConverter.java
+++ src/main/java/rabbit/open/orm/dml/RabbitValueConverter.java
@@ -1,6 +1,5 @@
 package rabbit.open.orm.dml;
 
-
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.ParseException;
@@ -17,7 +16,8 @@ import rabbit.open.orm.exception.RabbitDMLException;
 /**
  * 
  * DB字段值转换
- * @author	肖乾斌
+ * 
+ * @author 肖乾斌
  * 
  */
 public class RabbitValueConverter {
@@ -32,7 +32,7 @@ public class RabbitValueConverter {
 	 * 
 	 */
 	public static Object convert(Object value, FieldMetaData field) {
-        return convert(value, field, false);
+		return convert(value, field, false);
 	}
 
 	/**
@@ -42,7 +42,8 @@ public class RabbitValueConverter {
 	 * @param isReg
 	 * @return
 	 */
-	public static Object convert(Object value, FieldMetaData field, boolean isReg) {
+	public static Object convert(Object value, FieldMetaData field,
+			boolean isReg) {
 		if (null == value) {
 			return value;
 		}
@@ -66,96 +67,98 @@ public class RabbitValueConverter {
 			}
 			return value;
 		}
-		
+
 		if (field.isDate()) {
 			return getDate(value, field, isReg);
-			
+
 		}
 		return value;
 	}
 
-    private static Object convertArray(Object value) {
-        if (value instanceof Collection) {
-            return value;
-        }
-        List<Object> list = new ArrayList<>();
-        if (value.getClass().isArray()) {
-            Object[] arr = (Object[]) value;
-            for (Object v : arr) {
-                list.add(v);
-            }
-            return list;
-        }
-        list.add(value);
-        return list;
-    }
+	private static Object convertArray(Object value) {
+		if (value instanceof Collection) {
+			return value;
+		}
+		List<Object> list = new ArrayList<>();
+		if (value.getClass().isArray()) {
+			Object[] arr = (Object[]) value;
+			for (Object v : arr) {
+				list.add(v);
+			}
+			return list;
+		}
+		list.add(value);
+		return list;
+	}
 
-    @SuppressWarnings("unchecked")
-    private static Object getDate(Object value, FieldMetaData field, boolean isReg) {
-        if (null == value) {
-            return null;
-        }
-        if (value instanceof Collection || value.getClass().isArray()) {
-            List<Object> list = new ArrayList<>();
-            list.addAll((Collection<Object>) convertArray(value));
-            List<Object> dates = new ArrayList<>();
-            for (Object v : list) {
-                dates.add(convertDate(v, field, isReg));
-            }
-            return dates;
-        }
-        return convertDate(value, field, isReg);
-    }
+	@SuppressWarnings("unchecked")
+	private static Object getDate(Object value, FieldMetaData field,
+			boolean isReg) {
+		if (null == value) {
+			return null;
+		}
+		if (value instanceof Collection || value.getClass().isArray()) {
+			List<Object> list = new ArrayList<>();
+			list.addAll((Collection<Object>) convertArray(value));
+			List<Object> dates = new ArrayList<>();
+			for (Object v : list) {
+				dates.add(convertDate(v, field, isReg));
+			}
+			return dates;
+		}
+		return convertDate(value, field, isReg);
+	}
 
-    private static Object convertDate(Object value, FieldMetaData field, boolean isReg) {
-        SimpleDateFormat sdf = new SimpleDateFormat(field.getColumn().pattern());
-        if (value instanceof Date) {
-            String ds = sdf.format(value);
-            try {
-                return sdf.parse(ds);
-            } catch (ParseException e) {
-                return null;
-            }
-        } else {
-        	if (isReg) {
-        		return value;
-        	}
-            throw new RabbitDMLException("object[" + value
-                    + "] is not a instance of " + Date.class.getName());
-        }
-    }
-	
+	private static Object convertDate(Object value, FieldMetaData field,
+			boolean isReg) {
+		SimpleDateFormat sdf = new SimpleDateFormat(field.getColumn().pattern());
+		if (value instanceof Date) {
+			String ds = sdf.format(value);
+			try {
+				return sdf.parse(ds);
+			} catch (ParseException e) {
+				return null;
+			}
+		} else {
+			if (isReg) {
+				return value;
+			}
+			throw new RabbitDMLException("object[" + value
+					+ "] is not a instance of " + Date.class.getName());
+		}
+	}
+
 	/**
 	 * 
-	 * <b>Description: 值转换</b><br>.
+	 * <b>Description: 值转换</b><br>
 	 * @param data
 	 * @param type
-	 * @return	
+	 * @return
 	 * 
 	 */
 	public static Number cast(BigDecimal data, Class<?> type) {
-        if (Long.class.equals(type)) {
-            return data.longValue();
-        }
-        if (Integer.class.equals(type)) {
-            return data.intValue();
-        }
-        if (Short.class.equals(type)) {
-            return data.shortValue();
-        }
-        if (Double.class.equals(type)) {
-            return data.doubleValue();
-        }
-        if (Float.class.equals(type)) {
-            return data.floatValue();
-        }
-        if (BigInteger.class.equals(type)) {
-            return data.toBigInteger();
-        }
-        if (BigDecimal.class.equals(type)) {
-            return data;
-        }
+		if (Long.class.equals(type)) {
+			return data.longValue();
+		}
+		if (Integer.class.equals(type)) {
+			return data.intValue();
+		}
+		if (Short.class.equals(type)) {
+			return data.shortValue();
+		}
+		if (Double.class.equals(type)) {
+			return data.doubleValue();
+		}
+		if (Float.class.equals(type)) {
+			return data.floatValue();
+		}
+		if (BigInteger.class.equals(type)) {
+			return data.toBigInteger();
+		}
+		if (BigDecimal.class.equals(type)) {
+			return data;
+		}
 		throw new RabbitDMLException("not supported data type[" + type + "]");
 	}
-	
+
 }
