10c1da03a2b22d24e4c62d4ceb5697e95cef1035
RabbitOpen <709498191@qq.com>
2019-08-12 21:21:09 +0800

    v3.0

--- rabbit-orm-core/src/test/java/test/mapper/UserMapper.java
+++ rabbit-orm-core/src/test/java/test/mapper/UserMapper.java
@@ -1,36 +1,38 @@
 package test.mapper;
 
-import java.util.List;
-
-import rabbit.open.orm.common.annotation.FieldMapper;
-import rabbit.open.orm.common.annotation.Mapper;
-import rabbit.open.orm.common.annotation.SQLMapper;
+import rabbit.open.orm.common.annotation.NameSpace;
+import rabbit.open.orm.common.annotation.Param;
+import rabbit.open.orm.common.annotation.SQLName;
 import test.mapper.entity.MappingUser;
 
-@Mapper(MappingUser.class)
+import java.util.List;
+
+@NameSpace(MappingUser.class)
 public interface UserMapper {
 
-	@SQLMapper("getUserByNameAndId")
-	public MappingUser getUser(@FieldMapper("userId")long userId, @FieldMapper("username")String username);
-	
+	@SQLName("getUserByNameAndId")
+	public MappingUser getUser(@Param("userId") long userId, @Param("username")String username);
+
 	/**
 	 * <b>@description 根据id删除 </b>
 	 * @param userId
 	 * @return
 	 */
-	public long namedDelete(@FieldMapper("userId")long userId);
-	
+	public long namedDelete(@Param("userId")long userId);
+
 	/**
 	 * <b>@description 根据id更新name </b>
 	 * @param userId
 	 * @param name
 	 * @return
 	 */
-	public long updateNameById(@FieldMapper("userId")long userId, @FieldMapper("name")String name);
-	
-	public MappingUser getUserByJdbc(@FieldMapper("userId")long userId);
+	public long updateNameById(@Param("userId")long userId, @Param("name")String name);
 
-	@SQLMapper("getUserByJdbc")
-	public List<MappingUser> getUserByJdbcs(@FieldMapper("userId")long userId);
+	public MappingUser getUserByJdbc(@Param("userId")long userId);
+
+	@SQLName("getUserByJdbc")
+	public List<MappingUser> getUserByJdbcs(@Param("userId")long userId);
+
+	// update 、add、delete 测试
 
 }
