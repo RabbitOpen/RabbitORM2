e82e37be873dc0be4e52c7de081af4171e23bb60
RabbitOpen <709498191@qq.com>
2019-08-16 15:53:32 +0800

    v3.0 format code

--- rabbit-orm-datasource/src/main/java/rabbit/open/orm/datasource/AbstractConnection.java
+++ rabbit-orm-datasource/src/main/java/rabbit/open/orm/datasource/AbstractConnection.java
@@ -17,231 +17,203 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 
-public abstract class AbstractConnection implements Connection{
+public abstract class AbstractConnection implements Connection {
 
-	@Override
-	public <T> T unwrap(Class<T> iface) throws SQLException {
-		return null;
-	}
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		return false;
-	}
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
 
-	@Override
-	public CallableStatement prepareCall(String sql) throws SQLException {
-		return null;
-	}
+    @Override
+    public CallableStatement prepareCall(String sql) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public String nativeSQL(String sql) throws SQLException {
-		return null;
-	}
+    @Override
+    public String nativeSQL(String sql) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setCatalog(String catalog) throws SQLException {
-		
-	}
+    @Override
+    public void setCatalog(String catalog) throws SQLException {
 
-	@Override
-	public String getCatalog() throws SQLException {
-		return null;
-	}
+    }
 
-	@Override
-	public SQLWarning getWarnings() throws SQLException {
-		return null;
-	}
+    @Override
+    public String getCatalog() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void clearWarnings() throws SQLException {
-		
-	}
+    @Override
+    public SQLWarning getWarnings() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public Statement createStatement(int resultSetType, int resultSetConcurrency)
-			throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public void clearWarnings() throws SQLException {
 
-	@Override
-	public PreparedStatement prepareStatement(String sql, int resultSetType,
-			int resultSetConcurrency) throws SQLException {
-		
-		return null;
-	}
+    }
 
-	@Override
-	public CallableStatement prepareCall(String sql, int resultSetType,
-			int resultSetConcurrency) throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public Statement createStatement(int resultSetType, int resultSetConcurrency)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public Map<String, Class<?>> getTypeMap() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public PreparedStatement prepareStatement(String sql, int resultSetType,
+                                              int resultSetConcurrency) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-		
-		
-	}
+    @Override
+    public CallableStatement prepareCall(String sql, int resultSetType,
+                                         int resultSetConcurrency) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setHoldability(int holdability) throws SQLException {
-		
-		
-	}
+    @Override
+    public Map<String, Class<?>> getTypeMap() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public int getHoldability() throws SQLException {
-		
-		return 0;
-	}
+    @Override
+    public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
 
-	@Override
-	public Statement createStatement(int resultSetType,
-			int resultSetConcurrency, int resultSetHoldability)
-			throws SQLException {
-		
-		return null;
-	}
+    }
 
-	@Override
-	public PreparedStatement prepareStatement(String sql, int resultSetType,
-			int resultSetConcurrency, int resultSetHoldability)
-			throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public void setHoldability(int holdability) throws SQLException {
 
-	@Override
-	public CallableStatement prepareCall(String sql, int resultSetType,
-			int resultSetConcurrency, int resultSetHoldability)
-			throws SQLException {
-		
-		return null;
-	}
+    }
 
-	@Override
-	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
-			throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public int getHoldability() throws SQLException {
+        return 0;
+    }
 
-	@Override
-	public PreparedStatement prepareStatement(String sql, String[] columnNames)
-			throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public Statement createStatement(int resultSetType,
+                                     int resultSetConcurrency, int resultSetHoldability)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public Clob createClob() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public PreparedStatement prepareStatement(String sql, int resultSetType,
+                                              int resultSetConcurrency, int resultSetHoldability)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public Blob createBlob() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public CallableStatement prepareCall(String sql, int resultSetType,
+                                         int resultSetConcurrency, int resultSetHoldability)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public NClob createNClob() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public SQLXML createSQLXML() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public PreparedStatement prepareStatement(String sql, String[] columnNames)
+            throws SQLException {
+        return null;
+    }
 
-	@Override
-	public boolean isValid(int timeout) throws SQLException {
-		
-		return false;
-	}
+    @Override
+    public Clob createClob() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setClientInfo(String name, String value)
-			throws SQLClientInfoException {
-		
-		
-	}
+    @Override
+    public Blob createBlob() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setClientInfo(Properties properties)
-			throws SQLClientInfoException {
-		
-		
-	}
+    @Override
+    public NClob createNClob() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public String getClientInfo(String name) throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public SQLXML createSQLXML() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public Properties getClientInfo() throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public boolean isValid(int timeout) throws SQLException {
+        return false;
+    }
 
-	@Override
-	public Array createArrayOf(String typeName, Object[] elements)
-			throws SQLException {
-		
-		return null;
-	}
+    @Override
+    public void setClientInfo(String name, String value)
+            throws SQLClientInfoException {
 
-	@Override
-	public Struct createStruct(String typeName, Object[] attributes)
-			throws SQLException {
-		
-		return null;
-	}
+    }
 
-	@Override
-	public void setSchema(String schema) throws SQLException {
-		
-		
-	}
+    @Override
+    public void setClientInfo(Properties properties)
+            throws SQLClientInfoException {
 
-	@Override
-	public String getSchema() throws SQLException {
-		
-		return null;
-	}
+    }
 
-	@Override
-	public void abort(Executor executor) throws SQLException {
-		
-		
-	}
+    @Override
+    public String getClientInfo(String name) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public void setNetworkTimeout(Executor executor, int milliseconds)
-			throws SQLException {
-		
-		
-	}
+    @Override
+    public Properties getClientInfo() throws SQLException {
+        return null;
+    }
 
-	@Override
-	public int getNetworkTimeout() throws SQLException {
-		
-		return 0;
-	}
+    @Override
+    public Array createArrayOf(String typeName, Object[] elements)
+            throws SQLException {
+        return null;
+    }
 
-	
+    @Override
+    public Struct createStruct(String typeName, Object[] attributes)
+            throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setSchema(String schema) throws SQLException {
+
+    }
+
+    @Override
+    public String getSchema() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void abort(Executor executor) throws SQLException {
+
+    }
+
+    @Override
+    public void setNetworkTimeout(Executor executor, int milliseconds)
+            throws SQLException {
+
+    }
+
+    @Override
+    public int getNetworkTimeout() throws SQLException {
+        return 0;
+    }
+
+
 }
