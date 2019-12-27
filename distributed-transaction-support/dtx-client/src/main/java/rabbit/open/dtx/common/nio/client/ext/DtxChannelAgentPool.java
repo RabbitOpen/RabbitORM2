d665318697660e8497d158e8926ebc4d1e37c50f
RabbitOpen <709498191@qq.com>
2019-12-27 13:02:31 +0800

    remove unused import

--- distributed-transaction-support/dtx-client/src/main/java/rabbit/open/dtx/common/nio/client/ext/DtxChannelAgentPool.java
+++ distributed-transaction-support/dtx-client/src/main/java/rabbit/open/dtx/common/nio/client/ext/DtxChannelAgentPool.java
@@ -18,7 +18,6 @@ import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
