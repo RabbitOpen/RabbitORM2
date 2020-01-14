64030bd1417b0eaab46264794a251c6427a5abf4
RabbitOpen <709498191@qq.com>
2020-01-14 21:02:04 +0800

    优化选举算法，新增预选leader通告逻辑

--- distributed-transaction-support/dtx-algorithm/src/main/java/rabbit/open/algorithm/elect/protocol/ElectionArbiter.java
+++ distributed-transaction-support/dtx-algorithm/src/main/java/rabbit/open/algorithm/elect/protocol/ElectionArbiter.java
@@ -330,7 +330,6 @@ public class ElectionArbiter extends Thread implements Candidate {
 
 	@Override
 	public void onKittyReceived(HelloKitty kitty) {
-		logger.debug("hello kitty from[{}] is received!", kitty.getNodeId());
 		if (kitty.getNodeId().equals(this.leaderId)) {
 			updateLeaderLastActiveTime();
 		}
